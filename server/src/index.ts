da1f8cf537e36f0b824f43c077b606fce6b8cd97
Darshan Nagle <nagledarshan12@gmail.com>
2026-06-17 12:17:08 +0530

    Fix : Index

--- server/src/index.ts
+++ server/src/index.ts
@@ -38,8 +38,9 @@ const PORT = Number(process.env.PORT) || 5000;
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/status", (req, res) => res.send("OK"));
+app.use("/status", (req, res) => res.send("OK"));
 app.use("/api", routes);
+app.use("*", (req, res) => res.status(404).send("Not Found"));
 // DB Connection
 connectDB();
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
